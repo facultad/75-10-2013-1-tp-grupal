4491090340f1bb8c22203560e975ba05a0f38a26
Kimi <ezequiel.aceto@gmail.com>
2013-05-11 09:20:30 -0300

    CodingDojo Conversor Romano

---
+++ workspace/DojoFIUBA/src/ar/uba/fi/tecnicas/dojo/ConversorNumero.java
@@ -0,0 +1,15 @@
+package ar.uba.fi.tecnicas.dojo;
+
+public class ConversorNumero {
+	public int valor;
+	public char letra;
+	public int valorAnterior;
+	public char letraAnterior;
+	
+	public ConversorNumero(int valor, char letra, int valorAnterior, char letraAnterior){
+		this.valor = valor;
+		this.letra = letra;
+		this.valorAnterior = valorAnterior;
+		this.letraAnterior = letraAnterior;
+	}
+}
