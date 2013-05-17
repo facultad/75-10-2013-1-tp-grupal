511b4ba70d16c682aef3913877f80b7501cd1d91
Sebastian Goldberg <sebas.goldberg@gmail.com>
2013-05-17 18:24:49 -0300

    Se finaliza implementación de tests para clase Promocion. Su ejecución es exitosa.

--- workspace/HyperMarket/src/ar/uba/fi/tecnicasdedisenio/grupo8/hypermarket/promocion/test/PromocionTest.java
+++ workspace/HyperMarket/src/ar/uba/fi/tecnicasdedisenio/grupo8/hypermarket/promocion/test/PromocionTest.java
@@ -16,6 +16,18 @@ import ar.uba.fi.tecnicasdedisenio.grupo8.hypermarket.promocion.test.mock.Produc
 public class PromocionTest {
 
 	@Test
+	public void testPromocionNoAplicable() {
+		IProducto producto=new ProductoMock(1,10);
+		Promocion promocion=Promocion.crearPromocionNoAplicable();
+
+		for (int i = 1; i <= 10; i++) {
+			IItemVenta itemVenta=new ItemVentaMock(producto, i);
+			assertEquals(0,promocion.getImporteADescontar(itemVenta),0);
+			assertEquals(i,promocion.getCantidadProductosAplicaParaItemVenta(itemVenta),0);
+		}
+	}
+
+	@Test
 	public void testImporteADescontarPorUnidad() {
 		IProducto producto=new ProductoMock(1,10);
 		ICondicionPromocion condicion=new CondicionProducto(producto);
@@ -42,6 +54,7 @@ public class PromocionTest {
 		}
 	}
 	
+	@Test
 	public void testImporteADescontarCincuentaPorcientoEnSegundaUnidad() {
 		IProducto producto=new ProductoMock(1,100);
 		ICondicionPromocion condicion=new CondicionProducto(producto);
@@ -64,6 +77,7 @@ public class PromocionTest {
 	}
 
 
+	@Test
 	public void testImporteADescontarElQuintoGratis() {
 		IProducto producto=new ProductoMock(1,15);
 		ICondicionPromocion condicion=new CondicionProducto(producto);
@@ -76,13 +90,79 @@ public class PromocionTest {
 		assertEquals(0,promocion.getImporteADescontar(itemVenta),0);
 
 		itemVenta=new ItemVentaMock(producto, 5);
-		assertEquals(15*4,promocion.getImporteADescontar(itemVenta),0);
+		assertEquals(15,promocion.getImporteADescontar(itemVenta),0);
 
 		itemVenta=new ItemVentaMock(producto, 6);
-		assertEquals(15*5,promocion.getImporteADescontar(itemVenta),0);
+		assertEquals(15,promocion.getImporteADescontar(itemVenta),0);
 
 		itemVenta=new ItemVentaMock(producto, 14);
-		assertEquals(15*12,promocion.getImporteADescontar(itemVenta),0);
+		assertEquals(30,promocion.getImporteADescontar(itemVenta),0);
+	}
+	
+	@Test
+	public void testCantidadProductosAplicaParaItemVentaDescuentoPorUnidad() {
+		IProducto producto=new ProductoMock(1,10);
+		ICondicionPromocion condicion=new CondicionProducto(producto);
+		Promocion promocion=new Promocion(condicion, 1, 0.15);
+
+		for (int i = 1; i <= 10; i++) {
+			IItemVenta itemVenta=new ItemVentaMock(producto, i);
+			assertEquals(i,promocion.getCantidadProductosAplicaParaItemVenta(itemVenta),0);
+		}
+	}
+	
+	@Test
+	public void testCantidadProductosParaItemVentaNoAplica() {
+		IProducto producto=new ProductoMock(1,10);
+		ICondicionPromocion condicion=new CondicionProducto(producto);
+		Promocion promocion=new Promocion(condicion, 1, 0.15);
+		IItemVenta itemVenta=new ItemVentaMock(new ProductoMock(2,10),1);
+		try {
+			promocion.getCantidadProductosAplicaParaItemVenta(itemVenta);
+			fail();
+		} catch (PromocionNoAplicaParaItemVenta e) {
+			// La prueba es correcta porque se espera que lance la excepción 
+			// PromocionNoAplicaParaItemVenta. 
+		}
+	}
+	
+	@Test
+	public void testCantidadProductosAplicaParaItemVentaDescuentoEnTercerUnidad() {
+		IProducto producto=new ProductoMock(1,100);
+		ICondicionPromocion condicion=new CondicionProducto(producto);
+		Promocion promocion=new Promocion(condicion, 3, 0.25);
+		
+		IItemVenta itemVenta=new ItemVentaMock(producto, 1);
+		assertEquals(0,promocion.getCantidadProductosAplicaParaItemVenta(itemVenta),0);
+
+		itemVenta=new ItemVentaMock(producto, 2);
+		assertEquals(0,promocion.getCantidadProductosAplicaParaItemVenta(itemVenta),0);
+
+		itemVenta=new ItemVentaMock(producto, 3);
+		assertEquals(3,promocion.getCantidadProductosAplicaParaItemVenta(itemVenta),0);
+
+		itemVenta=new ItemVentaMock(producto, 32);
+		assertEquals(30,promocion.getCantidadProductosAplicaParaItemVenta(itemVenta),0);
+	}
+
+
+	@Test
+	public void testCantidadProductosAplicaParaItemVentaDescuentoEnNovenaUnidad() {
+		IProducto producto=new ProductoMock(1,15);
+		ICondicionPromocion condicion=new CondicionProducto(producto);
+		Promocion promocion=new Promocion(condicion, 9, 0.20);
+		
+		IItemVenta itemVenta=new ItemVentaMock(producto, 1);
+		assertEquals(0,promocion.getCantidadProductosAplicaParaItemVenta(itemVenta),0);
+
+		itemVenta=new ItemVentaMock(producto, 4);
+		assertEquals(0,promocion.getCantidadProductosAplicaParaItemVenta(itemVenta),0);
+
+		itemVenta=new ItemVentaMock(producto, 9);
+		assertEquals(9,promocion.getCantidadProductosAplicaParaItemVenta(itemVenta),0);
+
+		itemVenta=new ItemVentaMock(producto, 89);
+		assertEquals(81,promocion.getCantidadProductosAplicaParaItemVenta(itemVenta),0);
 	}
 
 }
