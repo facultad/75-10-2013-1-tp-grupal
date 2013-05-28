75494b42a0ba85d2b962c1d4849d8df8cda351ff
tongasoft <tongasoft@gmail.com>
2013-05-28 14:45:13 -0300

    Agregada Descripcion

--- workspace/HyperMarket/src/ar/uba/fi/tecnicasdedisenio/grupo8/hypermarket/caja/MedioPago.java
+++ workspace/HyperMarket/src/ar/uba/fi/tecnicasdedisenio/grupo8/hypermarket/caja/MedioPago.java
@@ -1,15 +1,21 @@
 package ar.uba.fi.tecnicasdedisenio.grupo8.hypermarket.caja;
 
 public class MedioPago implements IMedioPago{
-	private long id;
+	private long id = IdGenerator.getInstance().getNewId();
+	private String descripcion;
 	
-	public MedioPago (long id){
-		this.id=id;
+	public MedioPago (String desc){
+		this.descripcion=desc;
 	}
 	
 	@Override
 	public long getId() {
 		return this.id;
+	}
+
+	@Override
+	public String getDescripcion() {
+		return this.descripcion;
 	}
 
 }
